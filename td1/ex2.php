8e20dff46891d61840f0a73c45cebd00bc0fc365
21802635 <62337444+21802635@users.noreply.github.com>
2020-03-18 15:12:52 +0100

    Add files via upload

--- td1/ex2.php
+++ td1/ex2.php
@@ -1,9 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>ex2</title>
+    <meta charset="UTF-8">
+    <title>Hello</title>
 </head>
 <body>
-
+<a href="?Color=%2300FF00&size=15&message=Message">Message 1</a>
+<a href="?Color=%23ff0000&size=30&message=Message">Message 2</a>
+<a href="?Color=%230000FF&size=45&message=Message">Message 3</a>
+    <?php
+    echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
+    ?>
 </body>
 </html>
