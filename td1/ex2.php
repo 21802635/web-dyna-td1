77563922bb8e3ddaff4c8096a489d2d4a1fd8cc0
21802635 <62337444+21802635@users.noreply.github.com>
2020-03-19 11:57:27 +0100

    Add files via upload

--- td1/ex2.php
+++ td1/ex2.php
@@ -1,15 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
-    <title>Hello</title>
+  <meta charset="UTF-8">
+  <title>Ex2</title>
 </head>
+
+<form method="GET">
+<label for="color">Color :</label>
+<input type="color" id="html5colorpicker" onchange="clickColor(0, -1, -1, 5)" value="#ff0000" name="Color">
+<label for="size">Size :</label>
+<input type="number" value="" name="size" id="size">
+<label for="contenue">Contenue du message</label>
+<input type="text" value="" name="message" id="message">
+<input type="submit" value="Valider">
+</form>
+
 <body>
-<a href="?Color=%2300FF00&size=15&message=Message">Message 1</a>
-<a href="?Color=%23ff0000&size=30&message=Message">Message 2</a>
-<a href="?Color=%230000FF&size=45&message=Message">Message 3</a>
-    <?php
+  <a href="?Color=%23ff0000&size=15&message=Message1">Message 1</a>
+  <a href="?Color=%2300ff00&size=30&message=Message2">Message 2</a>
+  <a href="?Color=%230000ff&size=45&message=Message3">Message 3</a>
+  <a href="?&message=Sans couleurs">Sans couleur sans size</a>
+  <?php
+  if (empty($_GET)) {
+    echo "Erreur ! Veuillez entrer des paramètres";
+  }
+  if (empty($_GET["size"] and $_GET["Color"])) {
+    echo "<div style='font-size: 12px; color: black'>{$_GET["message"]}</div>";
+  }
+  else {
     echo "<div style='font-size: {$_GET["size"]}px; color: {$_GET["Color"]}'>{$_GET["message"]}</div>";
-    ?>
+  }
+  ?>
 </body>
 </html>
